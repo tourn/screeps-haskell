6ab3ef7c47660df075b5227a012a64f67c47058e
tourn <latzer.daniel@gmail.com>
2017-06-01 16:52:21 +0200

    Implement readCreep WIP

--- Makefile
+++ Makefile
@@ -5,5 +5,8 @@ upload: build
 	node upload-https.js $$EMAIL $$PASSWORD
 
 build:
-	docker run --rm -v `pwd`:/opt/workspace -w /opt/workspace tourn/screeps:latest stack build
+	docker run --rm -v `pwd`:/opt/workspace -w /opt/workspace tourn/screeps:latest stack build --allow-different-user
+
+run: build
+	node .stack-work/dist/x86_64-linux/Cabal-1.24.2.0_ghcjs/build/screeps-exe/screeps-exe.jsexe/all.js
 
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,22 +1,28 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Lib
     ( run
     ) where
 
 
 import qualified Data.JSString    as S
-import qualified GHCJS.Prim  as P (getProp)
+import qualified GHCJS.Prim  as P (getProp, fromJSArray)
 import qualified GHCJS.Types as T
 import qualified GHCJS.Foreign.Callback as C
 import           GHCJS.Marshal.Pure (pFromJSVal)
+import           GHCJS.Marshal.Internal (fromJSValUnchecked_pure)
 import           Control.Monad (forM_)
+import qualified Data.Map as Map
+import qualified JavaScript.Array as A
 
 foreign import javascript unsafe "console.log('hello hello')" js_sayHi :: IO ()
 foreign import javascript unsafe "{val: 42}" js_val :: T.JSVal
 foreign import javascript unsafe "'bla'" js_str :: S.JSString
 foreign import javascript unsafe "{name: \"myName\", energy: 42}" js_creep :: T.JSVal
+foreign import javascript unsafe "[{name: 'dummy', carry: { energy: 12}, pos: {x: 14, y: 15}, carryCapacity: 50 }, {name: 'dummy2', carry: {}, pos: {x: 45, y: 11}, carryCapacity: 100 }]" js_getcreeps_dummy :: T.JSVal
 
 foreign import javascript unsafe "module.exports.loop = $1" js_mainLoop :: C.Callback (IO()) -> IO()
 foreign import javascript unsafe "Game.creeps[$1].moveTo($2,$3);" js_moveAction :: S.JSString -> Int -> Int -> IO ()
+foreign import javascript unsafe "Game.creeps" js_getcreeps :: T.JSVal
 
 type CreepName = String
 type Position = (Int, Int)
@@ -28,15 +34,22 @@ data Action
 runAction :: Action -> IO ()
 runAction (MoveAction name (x, y)) = js_moveAction (S.pack name) x y
 
+type Resource = String
+type CreepInventory = Map.Map Resource Int
+
 data Creep
   = Creep
   { creepName :: String
-  , creepEnergyCapacity :: Int
-  , creepPosition :: (Int, Int)
+  , creepPos :: Position
+  , creepCarry :: CreepInventory
+  , creepCarryCapacity :: Int
   } deriving (Show)
 
 run :: IO()
 run = do
+  creepList <- P.fromJSArray js_getcreeps_dummy
+  creeps <- mapM readCreep creepList
+  putStrLn $ show creeps
   cb <- C.syncCallback C.ThrowWouldBlock tick
   js_mainLoop cb
   C.releaseCallback cb
@@ -47,24 +60,37 @@ tick = do
   --runAction $ MoveAction "myName" (10, 20)
   --putStrLn $ show creep
   js_sayHi
-  let room = Room 42
-  --let creeps =    [ Creep "creep1" 42 (2, 4)    , Creep "creep2" 8  (5, 10)    ]
-  let creeps =    [ Creep "Harvester1" 42 (2, 4) ]
-  let world = World room creeps
-  let actions = think world
+  creepList <- P.fromJSArray js_getcreeps_dummy
+  creeps <- mapM readCreep creepList
+  let actions = think creeps
   forM_ actions runAction
   putStrLn $ show actions
 
-think :: World -> [Action]
-think (World (Room r) creeps) = map makeAction creeps
+think :: [Creep] -> [Action]
+think creeps = map makeAction creeps
   where makeAction creep = MoveAction (creepName creep) (newPos creep)
-        newPos (Creep _ _ (x, y)) = (x + 1, y + 1)
+        newPos (Creep _ (x, y) _ _) = (x + 1, y + 1)
 
-data Room = Room Int deriving (Show)
-data World = World Room [Creep] deriving (Show)
 
 readCreep :: T.JSVal -> IO Creep
 readCreep v = do
   name   <- P.getProp v "name" 
-  energy <- P.getProp v "energy" 
-  return $ Creep (pFromJSVal name) (pFromJSVal energy) (0, 0)
+  carryCapacity <- P.getProp v "carryCapacity" 
+  js_carry <- P.getProp v "carry"
+  carry <- readCarry js_carry
+  js_pos <- P.getProp v "pos"
+  pos <- readPos js_pos
+  return $ Creep (pFromJSVal name) pos carry (pFromJSVal carryCapacity)
+
+readCarry :: T.JSVal -> IO CreepInventory
+readCarry v = do
+  return Map.empty
+  --js_energy <- P.getProp v "energy"
+  --let energy = pFromJSVal energy :: Int
+  --return $ Map.fromList ["energy", energy]
+
+readPos :: T.JSVal -> IO Position
+readPos v = do
+  x <- P.getProp v "x"
+  y <- P.getProp v "y"
+  return $ ((pFromJSVal x), (pFromJSVal y))
